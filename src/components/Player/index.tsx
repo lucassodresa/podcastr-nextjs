8707406d86cb6e2dcaad85b8943179f584f201ab
Lucas Sá <lucas.sodresa@gmail.com>
2021-04-24 14:04:03 +0100

    pause/play podcast implemented

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,47 +1,116 @@
-import styles from './styles.module.scss'
+import Image from 'next/image';
+import { useContext, useEffect, useRef } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
+import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 
-export function Player(){
+export function Player() {
+
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const { 
+        episodeList, 
+        currentEpisodeIndex, 
+        isPlaying, 
+        togglePlay,
+        setPlayingState
+    } = useContext(PlayerContext);
+
+
+    const episode = episodeList[currentEpisodeIndex];
+
+
+    useEffect(() => {
+        if (!audioRef.current) return;
+        
+        audioRef.current[isPlaying ? 'play' : 'pause']();
+    } ,[isPlaying]);
+
 
 
     return (
         <div className={styles.container}>
             <header>
-                <img src="/playing.svg" alt="Tocando agora"/>
+                <img src="/playing.svg" alt="Tocando agora" />
                 <strong>Tocando agora</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            { episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image
+                        width={592}
+                        height={592}
+                        src={episode.thumbnail}
+                        objectFit="cover"
+                    />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
 
-            <footer className={styles.empty}>
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            )}
+
+            <footer className={ !episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider}/>
+                        {episode ? (
+                            <Slider 
+                                trackStyle={{backgroundColor: '#04d361'}} 
+                                railStyle={{ backgroundColor: '#9f75ff' }}
+                                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}    
+
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        )}
                     </div>
                     <span>00:00</span>
                 </div>
 
+                { episode && (
+                    <audio 
+                    src={episode.url} 
+                    ref={audioRef} 
+                    autoPlay
+                    onPlay={()=> setPlayingState(true)}
+                    onPause={()=> setPlayingState(false)} 
+
+                />
+                )}
+
                 <div className={styles.buttons}>
-                    <button type="button">
-                        <img src="/shuffle.svg" alt="Embaralhar"/>
-                    </button>
-                
-                    <button type="button">
-                        <img src="/play-previous.svg" alt="Tocar anterior"/>
+                    <button type="button" disabled={!episode}>
+                        <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
 
-                    <button type="button" className={styles.playButton}>
-                        <img src="/play.svg" alt="Tocar"/>
+                    <button type="button" disabled={!episode}>
+                        <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
 
-                    <button type="button">
-                        <img src="/play-next.svg" alt="Tocar próximo"/>
+                    <button 
+                        type="button" 
+                        className={styles.playButton} 
+                        disabled={!episode}
+                        onClick={togglePlay}    
+                    >
+                        { isPlaying ? (
+                        <img src="/pause.svg" alt="Pausar" />
+                        ) : (
+                            <img src="/play.svg" alt="Tocar" />
+                        )}
                     </button>
-                
-                    <button type="button">
-                        <img src="/repeat.svg" alt="Repeat"/>
+
+                    <button type="button" disabled={!episode}>
+                        <img src="/play-next.svg" alt="Tocar próximo" />
+                    </button>
+
+                    <button type="button" disabled={!episode}>
+                        <img src="/repeat.svg" alt="Repeat" />
                     </button>
                 </div>
             </footer>
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,7 +3,7 @@ import ptBR from 'date-fns/locale/pt';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurantionToTimeString } from '../../utils/convertDurantionToTimeString';
 
@@ -29,7 +29,7 @@ type EpisodeProps = {
 
 
 export default function Episode({ episode }: EpisodeProps) {
-
+    
 
     return (
         <div className={styles.episode}>
@@ -60,8 +60,26 @@ export default function Episode({ episode }: EpisodeProps) {
 
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
+
+    const { data } = await api.get('episodes', {
+        params: {
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+      });
+
+    const paths = data.map( episode =>{
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    });
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
